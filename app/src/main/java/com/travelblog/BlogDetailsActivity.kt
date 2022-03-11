873041365914ebd4fc48019f1b3ed9933073c356
Pandeleanu Cosmin <cosmin.pandeleanu@yahoo.com>
2022-03-11 21:47:48 +0200

    Loading Data From the Internet

--- app/src/main/java/com/travelblog/BlogDetailsActivity.kt
+++ app/src/main/java/com/travelblog/BlogDetailsActivity.kt
@@ -1,48 +1,72 @@
 package com.travelblog
 
-import android.annotation.SuppressLint
 import android.os.Bundle
+import android.text.Html
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.google.android.material.snackbar.Snackbar
 import com.travelblog.databinding.ActivityBlogDetailsBinding
-
-private const val IMAGE_URL = "https://bitbucket.org/dmytrodanylyk/travel-blog-resources/raw/" +
-        "3436e16367c8ec2312a0644bebd2694d484eb047/images/sydney_image.jpg"
-private const val AVATAR_URL = "https://bitbucket.org/dmytrodanylyk/travel-blog-resources/raw/" +
-        "3436e16367c8ec2312a0644bebd2694d484eb047/avatars/avatar1.jpg"
+import com.travelblog.http.Blog
+import com.travelblog.http.BlogHttpClient
 
 class BlogDetailsActivity : AppCompatActivity() {
-    @SuppressLint("SetTextI18n")
+
+    private lateinit var binding: ActivityBlogDetailsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = ActivityBlogDetailsBinding.inflate(layoutInflater)
+        binding = ActivityBlogDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        Glide.with(this)
-            .load(IMAGE_URL)
-            .transition(DrawableTransitionOptions.withCrossFade())
-            .into(binding.imageMain)
+        binding.imageBack.setOnClickListener { finish() }
+
+        loadData()
+    }
+
+    private fun loadData() {
+        BlogHttpClient.loadBlogArticles(
+            onSuccess = { list: List<Blog> ->
+                runOnUiThread { showData(list[0]) }
+            },
+            onError = {
+                runOnUiThread{showErrorSnackbar()}
+            }
+        )
+    }
+
+    private fun showData(blog: Blog) {
+        binding.progressBar.visibility = View.GONE
+        binding.textTitle.text = blog.title
+        binding.textDate.text = blog.date
+        binding.textAuthor.text = blog.author.name
+        binding.textRating.text = blog.rating.toString()
+        binding.textViews.text = String.format("(%d views)", blog.views)
+        binding.textDescription.text = Html.fromHtml(blog.description)
+        binding.ratingBar.rating = blog.rating
 
         Glide.with(this)
-            .load(AVATAR_URL)
+            .load(blog.image)
+            .transition(DrawableTransitionOptions.withCrossFade())
+            .into(binding.imageMain)
+        Glide.with(this)
+            .load(blog.author.avatar)
             .transform(CircleCrop())
             .transition(DrawableTransitionOptions.withCrossFade())
             .into(binding.imageAvatar)
-//        binding.imageMain.setImageResource(R.drawable.sydney_image)
-//        binding.imageAvatar.setImageResource(R.drawable.avatar)
+    }
 
-        binding.textTitle.text = "G'day from Sydney"
-        binding.textDate.text = "August 2, 2019"
-        binding.textAuthor.text = "Grayson Wells"
-        binding.textRating.text = "4.4"
-        binding.textViews.text = "(2687 views)"
-        binding.textDescription.text = "Australia is one of the most popular travel destinations in the world."
-
-        binding.ratingBar.rating = 4.4f
-
-        binding.imageBack.setOnClickListener{ finish() }
+    private fun showErrorSnackbar() {
+        Snackbar.make(binding.root,
+            "Error during loading blog articles", Snackbar.LENGTH_INDEFINITE).run {
+            setActionTextColor(resources.getColor(R.color.orange500))
+            setAction("Retry") {
+                loadData()
+                dismiss()
+            }
+        }.show()
     }
 }
